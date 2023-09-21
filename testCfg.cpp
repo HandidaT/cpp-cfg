c28540f78dfb8e3e2c2d827889391f20a2160add
HandidaT <htromep@gmail.com>
2023-09-21 08:15:57 -0700

    initial commit

---
+++ testCfg.cpp
@@ -0,0 +1,17 @@
+#include <parseOptions.hpp>
+
+int main(int argc, char **argv) {
+  if(argc < 2) {
+    std::cout << "Usage: cfg.exe <input file>\n";
+    std::cout << "Example: cfg.exe configuration.txt\n";
+    exit(1);
+  }
+
+  auto options = parse_options(argv[1]);
+
+  if(!options.empty()){
+    //use options
+  }else{
+    std::cout << "Empty options list";
+  }
+}
